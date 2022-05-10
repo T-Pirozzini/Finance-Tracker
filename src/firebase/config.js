28616e4087f7482698da03b487765f2b28f1ddcd
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-05-10 13:26:58 -0700

    implement firebase and firestore config

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBti1fbItWm2Nsj0M_Pbr5h7b8oI9qm-8s",
+  authDomain: "financetracker-f047d.firebaseapp.com",
+  projectId: "financetracker-f047d",
+  storageBucket: "financetracker-f047d.appspot.com",
+  messagingSenderId: "33376930481",
+  appId: "1:33376930481:web:a286c7ecceb3ab6c107090"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init service
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+export { projectFirestore, projectAuth }
