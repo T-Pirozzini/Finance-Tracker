c4b1f3a3b984703d8107e094b26d95fe257c5cec
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-05-11 09:09:09 -0700

    create useLogout hook

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,6 +4,10 @@ export const AuthContext = createContext()
 
 export const authReducer = (state, action) => {
   switch (action.type) {
+    case 'LOGIN':
+      return { ...state, user: action.payload }
+    case 'LOGOUT':
+      return { ...state, user: null }
 
    default:
      return state
@@ -13,7 +17,8 @@ export const authReducer = (state, action) => {
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null
-  }) 
+  })
+  console.log('AuthContext state:', state) 
 
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
